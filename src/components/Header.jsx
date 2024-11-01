1814472ea9f3604b923a8aa23a8df5012cc133c9
Jonathan Andersson <2jonand@gmail.com>
2024-11-01 18:36:18 +0100

    testing of servicepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const location = useLocation();
 
   const navClass =
-      location.pathname === '/' ? '' :
+      location.pathname === '/' ? 'dark' :
       'dark';
 
   return (
--- src/components/services-page/ServiceHero.jsx
+++ src/components/services-page/ServiceHero.jsx
@@ -1,22 +1,46 @@
 import '../../assets/style/services-page/services-hero.scss';
 import '../../assets/style/globals/global.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
 
 const ServiceHero = () => {
   return (
-    <div className="service-hero">
-      <article className="service-hero-content">
-        <div className="bg-blur-blob left"></div>
-        <div className="bg-blur-blob center"></div>
-        <header className='heading-container'>
-          <h3 className="l-tag acc-txt">Tjänster</h3>
-          <h1 className="h-h dk-txt">
-            Vi erbjuder ett stort antal <span>tjänster</span> för alla dina behov
-          </h1>
-        </header>
-      </article>
-    </div>
-  )
-}
+    // <div className="service-hero">
+    //   <div className="service-overlay">
+    //     <article className="service-hero-content">
+    //       <FontAwesomeIcon className='bg-icon' icon={faScrewdriverWrench} />
+    //       {/* <div className="bg-blur-blob left"></div>
+    //       <div className="bg-blur-blob center"></div> */}
+    //       <header className='heading-container'>
+    //         <h3 className="l-tag lt-txt">Tjänster</h3>
+    //         <h1 className="h-h lt-txt">
+    //           Vi erbjuder ett stort antal <span className='pr-txt'>tjänster</span> för alla dina behov
+    //         </h1>
+    //       </header>
+    //     </article>
+    //   </div>
+    // </div>
+    // <div className="service-hero">
+    //   <div className="service-bg-overlay">
+    //     <article className="service-hero-content">
+    //       <FontAwesomeIcon className='bg-icon' icon={faScrewdriverWrench} />
+    //         {/* <div className="bg-blur-blob left"></div>
+    //         <div className="bg-blur-blob center"></div> */}
+    //         <header className='heading-container'>
+    //         <h3 className="h-sub acc-txt">Tjänster</h3>
+    //         <h1 className="h-h dk-txt">
+    //           Vi erbjuder ett stort antal tjänster för alla dina behov
+    //         </h1>
+    //       </header>
+    //     </article>
+    //   </div>
+    // </div>
+<header className='heading-container'>
+  <h1 className="h-h dk-txt">Tjänster</h1>
+  <p className="h-sub dk-txt">
+    Vi erbjuder ett stort antal tjänster för alla dina behov
+  </p>
+</header>
+)}
 
 export default ServiceHero;
