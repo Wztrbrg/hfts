917425394e0cff6cd9f1a0746f75a7bbce4b9a8b
Jonathan Andersson <2jonand@gmail.com>
2024-11-04 23:50:44 +0100

    almost done aboutPage, lots of styling fixes here and there

--- src/components/about-page/AboutContent.jsx
+++ src/components/about-page/AboutContent.jsx
@@ -26,11 +26,11 @@ const AboutContent = () => {
           <div className="bg-blur-blob left"></div>
           <div className="bg-blur-blob center"></div>
           <article className='about-card'>
-            <h3 className="m-h acc-txt">
+            <h3 className="lc-h acc-txt">
               Vårt uppdrag
             </h3>
             <div className="text">
-              <h1 className="l-h dk-txt">
+              <h1 className="m-h dk-txt">
                 Att skapa hållbara och trivsamma fastighetsmiljöer för framtiden
               </h1>
               <p className="l-p dk-txt">
@@ -40,17 +40,91 @@ const AboutContent = () => {
           </article>
 
           <article className='about-card'>
-            <h3 className="m-h acc-txt">
+            <h3 className="lc-h acc-txt">
               Våra värderingar
             </h3>
             <div className="text">
-              <h1 className="l-h dk-txt">
+              <h1 className="m-h dk-txt">
                 Hållbarhet, kvalitet och engagemang
               </h1>
               <p className="l-p dk-txt">
                 Hållbarhet är vårt ledord i alla delar av verksamheten. Genom att arbeta med noggrant utvalda leverantörer, miljövänliga material och energieffektiva metoder, strävar vi efter att minska miljöpåverkan. Kvalitet och engagemang är djupt förankrade i vårt team och driver oss att leverera tjänster som inte bara uppfyller våra kunders behov, utan också gör en positiv skillnad för miljön.
               </p>
             </div>
+          </article>
+        </section>
+
+        <section className="approach-section">
+          <header>
+            <h1 className="l-h dk-txt">Vår metod för hållbar fastighetsförvaltning</h1>
+          </header>
+          {/* <div className="h-divider"></div> */}
+          <section className="approach-grid">
+
+            <article className="approach-box">
+              <h1 className="l-h acc-txt">1.</h1>
+              <div className="text">
+                <h2 className="lc-h dp-txt">
+                  Energieffektiva lösningar
+                </h2>
+                <p className="lc-p dp-txt">
+                  Genom att använda modern teknik och innovativa lösningar ser vi till att fastigheterna vi förvaltar är så energieffektiva som möjligt. Vi erbjuder rådgivning och genomför åtgärder som sänker energiförbrukningen och därmed driftkostnaderna.
+                </p>
+              </div>
+            </article>
+            <div className="v-divider"></div>
+            <article className="approach-box">
+              <h1 className="l-h acc-txt">2.</h1>
+              <div className="text">
+                <h2 className="lc-h dp-txt">
+                  Miljövänliga material och arbetssätt
+                </h2>
+                <p className="lc-p dp-txt">
+                  För att minimera vår miljöpåverkan använder vi oss av miljöcertifierade och hållbara material i allt från fastighetsskötsel till renovering och underhåll.
+                </p>
+              </div>
+            </article>
+            <div className="v-divider"></div>
+            <article className="approach-box">
+              <h1 className="l-h acc-txt">3.</h1>
+              <div className="text">
+                <h2 className="lc-h dp-txt">
+                  Digitalisering för en bättre miljö
+                </h2>
+                <p className="lc-p dp-txt">
+                  Vi använder digitala verktyg och system för att optimera vårt arbete och minska pappersförbrukningen. Genom digital kommunikation och fjärrövervakning av fastighetssystem kan vi säkerställa en effektiv och resursbesparande förvaltning.
+                </p>
+              </div>
+            </article>
+
+          </section>
+        </section>
+
+        <section className="why-section">
+          <div className="bg-img-overlay"></div>
+          <article className="why-content">
+            <header className='heading-container'>
+              <h1 className="h-h lt-txt">
+                Varför välja <span className="pr-txt">Hållbar Fastighetsservice Syd</span>?
+              </h1>
+            </header>
+            <div className="text">
+              <p className="h-sub lt-txt">
+                Att välja Hållbar Fastighetsservice Syd innebär att du väljer en långsiktig partner som sätter miljön och kvaliteten i första rummet. Vi tror på att skapa fastighetsvärden som håller över tid, både ekonomiskt och ekologiskt.
+              </p>
+              <ul>
+                <li>
+                  <p className="l-p lt-txt">Lång erfarenhet inom fastighetsförvaltning</p>
+                </li>
+                <li>
+                  <p className="l-p lt-txt">Miljöanpassade lösningar för en minskad klimatpåverkan</p>
+                </li>
+                <li>
+                  <p className="l-p lt-txt">Kundnära service med engagemang och lyhördhet för våra kunders behov</p>
+                </li>
+              </ul>
+            </div>
+
           </article>
         </section>
       </div>
