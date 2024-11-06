512ddd3d01b89ae4dcd9929bb0a0d6d05d140121
Jonathan Andersson <2jonand@gmail.com>
2024-11-06 22:20:28 +0100

    changed some of the hero image stylings

--- src/components/contact-page/ContactHero.jsx
+++ src/components/contact-page/ContactHero.jsx
@@ -1,12 +1,12 @@
-import '../../assets/style/about-page/about-hero.scss';
+import '../../assets/style/contact-page/contact-hero.scss';
 import '../../assets/style/globals/global.scss';
 
 
 const ContactHero = () => {
   return (
-    <div className="about-hero">
+    <div className="contact-hero">
       <div className="bg-img-overlay"></div>
-      <article className="about-hero-content">
+      <article className="contact-hero-content">
         <div className="bg-blur-blob left"></div>
         <div className="bg-blur-blob center"></div>
         <header className='heading-container'>
