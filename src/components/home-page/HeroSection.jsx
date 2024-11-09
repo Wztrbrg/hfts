3b9fcce8462942b1fdf2557a8147d720d761d394
Jonathan Andersson <2jonand@gmail.com>
2024-11-10 00:34:44 +0100

    added animation to homePAge hero section

--- src/components/home-page/HeroSection.jsx
+++ src/components/home-page/HeroSection.jsx
@@ -1,11 +1,36 @@
 import '../../assets/style/home-page/hero-section.scss';
 import { Link } from 'react-router-dom';
+import React, { useRef, useEffect, useState } from 'react';
 
 const HeroSection = () => {
+  const heroRef = useRef(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          setIsInView(entry.isIntersecting);
+        });
+      },
+      { threshold: 0 }
+    );
+
+    if (heroRef.current) {
+      observer.observe(heroRef.current);
+    }
+
+    return () => {
+      if (heroRef.current) {
+        observer.unobserve(heroRef.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <section className="hero-section">
-        <div className="hero-section-overlay bg-dk-overlay"></div>
+        <div className={`hero-section-overlay ${isInView ? 'animate' : ''}`} ref={heroRef}></div>
           <article className="hero-content">
             <h1 className="hero-heading h-h lt-txt">
               Välkommen till Hållbar <span className="pr-txt">Fastighetsservice Syd</span> 
