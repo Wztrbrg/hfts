0b922a95d8316d606eb32aa7ae73a4841451f326
Jonathan Andersson <2jonand@gmail.com>
2024-11-14 23:31:00 +0100

    some more responsive styling updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import '../assets/style/header.scss';
 import logo from '../assets/images/site-logo.png';
 
 const Header = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  }
+
   return (
     <>
-      <header className={"top-header"}>
+      <header className={`top-header ${isOpen ? "open" : ""}`}>
+
+        <button className={`menu-btn ${isOpen ? "close" : ""}`} onClick={toggleMenu}>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+        </button>
+
         <nav className="top-nav">
           <NavLink to={'/'}>
-            <img src={logo} alt="logo" className="site-logo" />
+            <img src={logo} alt="logo" className="site-logo" onClick={closeMenu}/>
           </NavLink>
-          <ul className="site-links">
+          <ul className={`site-links ${isOpen ? "open" : ""}`}>
             <li className="link-item">
-              <NavLink to={'/'} exact='true' className="site-link nav-txt lt-txt" activeclassname="active">hem</NavLink>
+              <NavLink to={'/'} exact='true' className="site-link nav-txt lt-txt" activeclassname="active" onClick={closeMenu}>hem</NavLink>
             </li>
             <li className="link-item">
-              <NavLink to={'/tjanster'} className="site-link nav-txt lt-txt" activeclassname="active">tjänster</NavLink>
-            </li>
-            {/* <li className="link-item">
-              <NavLink to={'/bostader-och-lokaler'} className="site-link nav-txt lt-txt" activeclassname="active">lediga bostäder & lokaler</NavLink>
-            </li> */}
-            <li className="link-item">
-              <NavLink to={'/om-oss'} className="site-link nav-txt lt-txt" activeclassname="active">om oss</NavLink>
+              <NavLink to={'/tjanster'} className="site-link nav-txt lt-txt" activeclassname="active" onClick={closeMenu}>tjänster</NavLink>
             </li>
             <li className="link-item">
-              <NavLink to={'/kontakt'} className="site-link nav-txt lt-txt" activeclassname="active">kontakt</NavLink>
+              <NavLink to={'/om-oss'} className="site-link nav-txt lt-txt" activeclassname="active" onClick={closeMenu}>om oss</NavLink>
+            </li>
+            <li className="link-item">
+              <NavLink to={'/kontakt'} className="site-link nav-txt lt-txt" activeclassname="active" onClick={closeMenu}>kontakt</NavLink>
             </li>
             <li className="link-item ">
               <NavLink to={'/felanmalan'}>
-                <button className="m-btn prim">felanmälan</button>
+                <button className="m-btn prim" onClick={closeMenu}>felanmälan</button>
               </NavLink>
             </li>
           </ul>
