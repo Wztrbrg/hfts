3ab6dfa408d14a7b0a624a59413373d77005faf9
Jonathan Andersson <2jonand@gmail.com>
2025-03-16 21:56:46 +0100

    forgot some review text

--- src/components/home-page/CardSlider.jsx
+++ src/components/home-page/CardSlider.jsx
@@ -29,7 +29,7 @@ const CardSlider = () => {
 
   const cards = [
     { id: 1, image: "https://lh3.googleusercontent.com/a/ACg8ocL92pA2GHyvdOrcy5IsB-9t6mIwxB80WHZHNi24KG_9jVWwew=w54-h54-p-rp-mo-br100", title: "Jonathan Andersson", desc: "Utmärkt service och trevligt bemötande!" },
-    { id: 2, image: "https://lh3.googleusercontent.com/a/ACg8ocLwgGk0jhyGri_tuw6Vm2bmDWlmpKuIY6fPf-M1AezUKF8RKw=w54-h54-p-rp-mo-br100", title: "titel", desc: "Jättebra service och trevligt bemötande! Håller alltid vad dom lovar" },
+    { id: 2, image: "https://lh3.googleusercontent.com/a/ACg8ocLwgGk0jhyGri_tuw6Vm2bmDWlmpKuIY6fPf-M1AezUKF8RKw=w54-h54-p-rp-mo-br100", title: "Rene Makula", desc: "Professionell och pålitlig fastighetsservice med snabb respons och hög kvalitet. Arbetet utförs noggrant och med stor omtanke om både fastigheten och hyresgästerna. Rekommenderas starkt!!!" },
     { id: 3, image: "https://lh3.googleusercontent.com/a-/ALV-UjXeGrCOO7q_SdRQVEF94PHkEndiMF8VB75uF_Uo-qNFTVmweAYY=w54-h54-p-rp-mo-br100", title: "Johan Leskelä", desc: "Jag är otroligt nöjd med Hållbar Fastighetsservice Syd AB. De är pålitliga, professionella och håller alltid hög kvalitet." },
     { id: 4, image: "https://lh3.googleusercontent.com/a-/ALV-UjXgJ8kZfW7vAR1IRTATQikeVau9G01He1eEeZaX85kjLV5zytq0=w54-h54-p-rp-mo-br100", title: "Malin Nordin", desc: "Fick hjälp att få tillbaka värmen i huset samt tips på hur vi kan få ner energiförbrukningen" },
     { id: 5, image: "https://lh3.googleusercontent.com/a-/ALV-UjUza5ZhVy53Sh-B6Rwvz_IJ6gkU4wJCA31g4n0V4D2SRjw6li3c=w54-h54-p-rp-mo-br100", title: "Kristian Trpkoski", desc: "Kompetenta konsulter med väldigt bra service. Kommer fortsätta samarbetet 5/5" },
